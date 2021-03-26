bc308528afdff87de110173bee3b80a86c35a8c5
leeedev <leeesangheee@gmail.com>
2021-03-27 01:05:27 +0900

    Create Queries with Arguments

--- graphql/db.js
+++ graphql/db.js
@@ -1,18 +1,18 @@
 export const people = [
   {
-    id: 0,
+    id: "0",
     name: "apple",
     age: 18,
     gender: "female",
   },
   {
-    id: 1,
+    id: "1",
     name: "orange",
     age: 19,
     gender: "male",
   },
   {
-    id: 2,
+    id: "2",
     name: "kiwi",
     age: 22,
     gender: "female",
@@ -20,6 +20,6 @@ export const people = [
 ];
 
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
+  const filteredPeople = people.filter((person) => person.id === String(id));
   return filteredPeople[0];
 };
