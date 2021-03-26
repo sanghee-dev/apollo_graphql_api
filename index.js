5014ab2de3c08328d7574a51a683c8d693c162e0
leeedev <leeesangheee@gmail.com>
2021-03-26 22:06:46 +0900

    Create the first Query and Resolver

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("index");
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+
+const server = new GraphQLServer({
+  typeDefs: "./graphql/schema.graphql",
+  resolvers: resolvers,
+});
+
+server.start(() => console.log("Server Running"));
